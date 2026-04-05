d0f65f927ac9955c41f24b8ad86e5981cbd3f02f
Sharipov Huseyn <huseynsharipov666@gmail.com>
2026-04-05 13:21:32 +0700

    building

--- vite.config.js
+++ vite.config.js
@@ -17,47 +17,44 @@
 // })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import compression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // Оптимизация SVG
+        plugins: ['@svgr/plugin-svgo'],
+        svgoConfig: {
+          plugins: [
+            'preset-default',
+            'removeViewBox',
+            'removeDimensions',
+            'minifyStyles',
+          ],
+        },
+      },
+    }),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
   build: {
-    // Увеличиваем лимит памяти
     chunkSizeWarningLimit: 1000,
-    // Оптимизируем сборку
     rollupOptions: {
       output: {
         manualChunks: {
-          // Разделяем большие зависимости на отдельные чанки
           vendor: ['react', 'react-dom', 'react-router-dom'],
           antd: ['antd'],
-          icons: ['@ant-design/icons', 'react-icons'],
           redux: ['@reduxjs/toolkit', 'react-redux'],
         },
       },
     },
-    // Уменьшаем количество потоков для экономии памяти
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    // Отключаем source maps для продакшена
     sourcemap: false,
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://api.gochange.tech',
-        changeOrigin: true,
-      },
-    },
-  },
-  // Оптимизация зависимостей
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', 'antd'],
-    exclude: ['large-svg-files'], // если есть
+    // Увеличиваем таймаут для больших файлов
+    assetsInlineLimit: 4096,
   },
 })
